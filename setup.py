07d1a2a794ddb4e481e2e47e6bb623749ce0a480
Yumi_Amahane <51439946+Yumi-Amahane@users.noreply.github.com>
2020-09-11 12:44:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,10 @@ setuptools.setup(
     version="0.1.0",
     author="Yumi-Amahane",
     description="Making Mage Program",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/Yumi-Amahane/MakingMage",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    
+    packages=["MakingMage"],
     entry_points = {
         'console_scripts': ['MakingMage = MakingMage.MakingMage:main']
-    },
-    python_requires='>=3.8.5',
+    }
 )
