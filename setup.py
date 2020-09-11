497f748befe47f9309e69664f1cf4ca7087e3a77
Yumi_Amahane <51439946+Yumi-Amahane@users.noreply.github.com>
2020-09-11 14:56:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ import setuptools
 
 setuptools.setup(
     name="MakingMage",
-    version="0.1.7",
+    version="0.1.8",
     author="Yumi-Amahane",
     description="Making Mage Program",
     url="https://github.com/Yumi-Amahane/MakingMage",
-    packages=setuptools.find_packages(),
-    scripts=['MakingMage/MakingMage.py']
+    packages=setuptools.find_packages()
 )
